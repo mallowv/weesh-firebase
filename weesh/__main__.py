3332ab1218e1d449256707c8b6579c81cbc13813
Mallow <72262190+mallowv@users.noreply.github.com>
2022-06-13 12:40:47 +0700

    🛠 Added new features - Added code to serve static UI files ♻ Refactored - Moved the make_shortcut function above other listeners - Chnaged from using uuid as id to custom random string function - Added id argument to make_shortcut response

--- weesh/__main__.py
+++ weesh/__main__.py
@@ -5,11 +5,13 @@
 
 from os import getenv
 from typing import Optional
-from uuid import uuid4
+import random
+import string
 import json
 
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 import firebase_admin
@@ -24,11 +26,50 @@ db = firestore.client()
 
 app = FastAPI()
 
+def rand_str(chars = string.ascii_uppercase + string.digits, length=10):
+    """
+    generate random string for id if user doesn't provide one
+    """
+    return ''.join(random.choice(chars) for _ in range(length))
+
 
 class NewURL(BaseModel):
     id: Optional[str]
     url: str
 
+
+app.mount("/static", StaticFiles(directory="ui"), name="ui")
+
+@app.get("/")
+async def root():
+    with open("ui/index.html") as f:
+        return HTMLResponse(content=f.read(), status_code=200)
+
+@app.post("/shorten")
+async def make_shortcut(url: NewURL, req: Request):
+    """
+    Make a new entry in the db for new url with optional id.
+    :param req:
+    :param url:
+    :return: RedirectResponse | HTTPException
+    """
+    if url.id:
+        doc_ref = db.collection('urls').document(url.id)
+
+        doc = doc_ref.get()
+        if doc.exists:
+            raise HTTPException(
+                status_code=409,
+                detail=f"the url 'https://{req.url.hostname}/{url.id}' already exists"
+            )
+        shorten = {"url": url.url, "id": url.id}
+    else:
+        shorten = {"url": url.url, "id": rand_str()}
+
+    doc_ref = db.collection("urls").document(shorten["id"])
+    doc_ref.set(shorten)
+    print(shorten)
+    return {"url": f"https://{req.url.hostname}/{shorten['id']}", "id": shorten["id"]}
 
 @app.get("/{url}")
 async def read_item(url: str):
@@ -61,29 +102,3 @@ async def read_item_raw(url: str):
         return {"id": doc.to_dict()["id"], "url": doc.to_dict()["url"]}
 
     raise HTTPException(status_code=404, detail="That url does not exist")
-
-
-@app.post("/")
-async def make_shortcut(url: NewURL, req: Request):
-    """
-    Make a new entry in the db for new url with optional id.
-    :param req:
-    :param url:
-    :return: RedirectResponse | HTTPException
-    """
-    if url.id:
-        doc_ref = db.collection('urls').document(url.id)
-
-        doc = doc_ref.get()
-        if doc.exists:
-            raise HTTPException(
-                status_code=409,
-                detail=f"the url 'https://{req.url.hostname}/{url.id}' already exists"
-            )
-        shorten = {"url": url.url, "id": url.id}
-    else:
-        shorten = {"url": url.url, "id": uuid4().hex}
-
-    doc_ref = db.collection("urls").document(shorten["id"])
-    doc_ref.set(shorten)
-    return {"url": f"https://{req.url.hostname}/{shorten['id']}"}
